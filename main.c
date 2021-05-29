70f988d48e433e3cd9ea30e6ee1b736cfda3e77b
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-29 13:02:32 +0900

    Finish AutoClick1.0

--- main.c
+++ main.c
@@ -5,21 +5,7 @@
 #include <winuser.h>
 
 void printing();
-int click(int time) {
-    POINT pt;
-    while (1) {
-        if (GetAsyncKeyState(VK_F3)) {
-            return 0;
-        } else if (GetAsyncKeyState(VK_F2)) {
-            return 1;
-        } else {
-            GetCursorPos(&pt);
-            mouse_event(MOUSEEVENTF_LEFTDOWN, pt.x, pt.y, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, pt.x, pt.y, 0, 0);
-            Sleep(time);
-        }
-    }
-}
+int click(int time);
 
 
 int main() {
@@ -49,12 +35,28 @@ int main() {
     return 1;
 }
 
+int click(int time) {
+    POINT pt;
+    while (1) {
+        if (GetAsyncKeyState(VK_F3)) {
+            return 0;
+        } else if (GetAsyncKeyState(VK_F4)) {
+            return 1;
+        } else {
+            GetCursorPos(&pt);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, pt.x, pt.y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, pt.x, pt.y, 0, 0);
+            Sleep(time);
+        }
+    }
+}
+
 void printing() {
     system("cls");
     printf("AutoClicker1.0\n");
     printf("===Description===\n");
     printf("1. F1 : Start Clicking\n");
-    printf("2. F2 : Stop Clicking\n");
+    printf("2. F4 : Stop Clicking\n");
     printf("3. F3 : Exit AutoClicker1.0\n");
 }
 

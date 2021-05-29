81e0b2483fc035d39011ccb2e38e46dd481f37a8
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-29 12:54:00 +0900

    edit

--- main.c
+++ main.c
@@ -4,21 +4,42 @@
 #include <windows.h>
 #include <winuser.h>
 
-void clicking(int time);
 void printing();
+int click(int time) {
+    POINT pt;
+    while (1) {
+        if (GetAsyncKeyState(VK_F3)) {
+            return 0;
+        } else if (GetAsyncKeyState(VK_F2)) {
+            return 1;
+        } else {
+            GetCursorPos(&pt);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, pt.x, pt.y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, pt.x, pt.y, 0, 0);
+            Sleep(time);
+        }
+    }
+}
+
 
 int main() {
     int time = 0;
+    int check = 1;
 
-    while (!GetAsyncKeyState(VK_F3)) {
-        printing();
+    while (check != 0) {
+        if (check == 1) {
+            printing();
 
-        printf("Input Time(1000 = 1s) : ");
-        scanf("%d", &time);
-        printf("Listening...\n");
+            printf("Input Time(1000 = 1s) : ");
+            scanf("%d", &time);
+            printf("Listening...\n");
+
+            check = -1;
+        }
+
 
         if (GetAsyncKeyState(VK_F1)) {
-            clicking(time);
+            check = click(time);
         }
     }
 
@@ -37,17 +58,3 @@ void printing() {
     printf("3. F3 : Exit AutoClicker1.0");
 }
 
-// time = 1000 1초
-void clicking(int time) {
-    POINT pt;
-    while (1) {
-        if (GetAsyncKeyState(VK_F2)) {
-            return;
-        }
-        GetCursorPos(&pt);
-        mouse_event(MOUSEEVENTF_LEFTDOWN, pt.x, pt.y, 0, 0);
-        mouse_event(MOUSEEVENTF_LEFTUP, pt.x, pt.y, 0, 0);
-        Sleep(time);
-    }
-    return;
-}
